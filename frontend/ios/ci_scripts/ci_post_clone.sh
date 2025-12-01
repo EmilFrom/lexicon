ea2b1aebde442b838cabdc0e5d44d2918e0975da
EmilFrom <emilfrom@gmail.com>
2025-12-01 12:29:45 +0100

    Update ci_post_clone.sh

--- frontend/ios/ci_scripts/ci_post_clone.sh
+++ frontend/ios/ci_scripts/ci_post_clone.sh
@@ -22,9 +22,11 @@ yarn install
 yarn add expo
 
 # Generate the missing API files before building
+cd ../
 echo "Generating API files..."
 yarn generate
 # ===================
+cd frontend/
 
 # 3. Install iOS Pods
 #npx build -p ios --profile production --local
